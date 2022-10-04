c2dce4c7900f39fe9bd13afa0b4116c92194206b
Chirag <chiragsarvaiya19124@gmail.com>
2022-10-04 11:14:43 +0530

    First Commit

---
+++ StudentPIVPUB.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    int adno;
+    string sname;
+    
+};
+int main()
+{
+    Student obj;
+    obj.takeData();
+    obj.cTotal();
+}
+
+
+
+    

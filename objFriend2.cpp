265dafc4dc9352f6dce39437d40aaf4c188a82d9
Sarvaiya Chirag <114641852+Sarvaiya1314@users.noreply.github.com>
2022-10-13 11:21:14 +0530

    Add files via upload

--- objFriend2.cpp
+++ objFriend2.cpp
@@ -21,21 +21,21 @@ class Loc
         cout<<"\n longitude :"<<longitude<<"";
         cout<<"\n latitude :"<<latitude<<"\n";
     }
-    friend  Loc operator+(Loc,Loc);
+    friend  Loc operator*(Loc,Loc);
     
 };
-Loc operator+(Loc ob1,Loc ob2)
+Loc operator*(Loc ob1,Loc ob2)
 {
     Loc temp;
-    temp.longitude=ob1.longitude+ob2.longitude;
-    temp.latitude=ob1.latitude+ob2.latitude;
+    temp.longitude=ob1.longitude*ob2.longitude;
+    temp.latitude=ob1.latitude*ob2.latitude;
 }
 int main()
 {
     Loc ob1(10,20),ob2(5,30),ob3;
     ob1.show();
     ob2.show();
-    ob3=ob1+ob2;
+    ob3=ob1*ob2;
     ob3.show();
     return 0;
 }

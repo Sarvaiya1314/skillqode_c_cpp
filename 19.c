e96daab3d0f3c53a987db7d865518f8b6ee5c419
Sarvaiya Chirag <114641852+Sarvaiya1314@users.noreply.github.com>
2022-11-19 11:38:04 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+//#define SIZE 10
+//#define pr printf
+int a[10],top=-1,val;
+void push(int val)
+{
+    
+    if(top>=10)
+    {
+        printf("Stack Overflow");
+
+    }
+    else{
+       // printf("enter Number : ");
+       // scanf("%d",val);
+        a[++top]=val;
+    }
+
+}
+int pop(int x)
+{
+    if(top<0)
+    {
+        printf("stack underflow");
+    }
+    else
+    {
+        x=a[top--];
+    }
+    return x;
+}
+int main()
+{
+    int x;
+    push(10);
+    push(20);
+    pop(10);
+    pop(x);
+    //printf("\n Deleted Value : %d ",x);
+    printf("\n Value : %d",val);
+    
+}

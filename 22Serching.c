f14d8d4cb0a6b9e760326d14346f3019f953348b
Sarvaiya Chirag <114641852+Sarvaiya1314@users.noreply.github.com>
2022-11-22 11:33:58 +0530

    Add files via upload

---
+++ 22Serching.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>  
+int main()
+{
+    int a[20],i,x,n;
+    printf(" how many enter your numbers  :");
+    scanf("%d",&n);
+     
+    
+    for(i=0;i<=n;i++)
+    {
+        a[i]=rand()%50+1;
+        printf("\n Your value is : %d",a[i]);
+    }
+    printf("\nEnter element to search  :");
+    scanf("%d",&x);
+     
+    for(i=0;i<n;++i)
+        if(a[i]==x)
+            break;
+     
+    if(i<n)
+        printf("your possion number  is %d  ",i+1);
+    else
+        printf("Element not found  ");
+  
+    return 0;
+}

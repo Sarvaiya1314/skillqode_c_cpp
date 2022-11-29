a11c5f8e934a565921c6a5d958eecbb2021e8460
Sarvaiya Chirag <114641852+Sarvaiya1314@users.noreply.github.com>
2022-11-29 11:38:28 +0530

    Add files via upload

---
+++ Stack/18.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Insert(int a[])
+{
+    for(int  i=0; i<5; i++)
+    {
+     printf("Enter  A[%d] : ",i);
+     scanf("%d",&a[i]);
+    }
+}
+void print(int a[])
+{
+    for(int i=0; i<5; i++)
+    {
+    printf("\n %d - %u",a[i]);
+    }
+}
+void last_insert(int a[])
+{
+    
+    a[6]=66;
+}
+int main()
+{
+    int a[6];
+
+    Insert(a);
+    print(a);
+    last_insert(a);
+    
+
+    for(int i=0; i<=6; i++)
+    printf("\n %d - %u ",a[i],a[i]);
+
+    
+   
+   
+}
